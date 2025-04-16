0664ecec2453434309589f75d047639e71eb0eae
nathiya-roriri <nathiya@roririsoft.com>
2025-04-16 17:07:24 +0530

    modify erp

--- academy/modal/trainee.php
+++ academy/modal/trainee.php
@@ -63,7 +63,8 @@
                                     <div class="form-group pb-3">
                                         <label for="course" class="form-label"><b>Duration</b><span class="text-danger">*</span></label>
                                         <select id="duration" class="form-select">
-                                            <option value="">-- Select Duration --</option>                                           
+                                            <option value="">-- Select Duration --</option>
+                                           
                                         </select>
                                         <div id="durationError" style="color: red" class="error-message">Duration is required.</div>
                                     </div>
@@ -74,12 +75,21 @@
                                         <input type="number" class="form-control" placeholder="Enter Fee Amount" name="fee" id="fee" disabled>
                                     </div>
                                 </div>
+
                                 <div class="col-md-6">
                                     <div class="form-group pb-3">
                                         <label for="discount" class="form-label"><b>Discount Amount (₹)</b></label>
                                         <input type="text" class="form-control" name="discount" id="discount" readonly>
                                     </div>
                                 </div>
+
+                                <!-- <div class="col-md-6">
+                                    <div class="form-group pb-3">
+                                        <label for="fee_amount" class="form-label"><b>Fee Amount</b><span class="text-danger">*</span></label>
+                                        <input type="number" class="form-control" placeholder="Enter Fee Amount" name="fee_amount" id="fee_amount">
+                                        <div id="fee_amountError" style="color: red" class="error-message">Fee Amount is required.</div>
+                                    </div>
+                                </div> -->
                             </div>
                         </div>     
                         <div class="col-12 mt-2">
@@ -138,3 +148,189 @@
         </div>
     </div>
 </div>
+
+<script>
+
+    
+$(document).ready(function () {
+    // When course is selected
+    $('#course').on('change', function () {
+        let courseId = $(this).val();
+
+        // Clear previous options
+        $('#duration').html('<option value="">-- Select Duration --</option>');
+        $('#fee').val('');
+        $('#discount').val('');
+
+        if (courseId) {
+            $.ajax({
+                url: 'trainee.php',
+                type: 'POST',
+                data: {
+                    type: 'get_durations',
+                    course_id: courseId
+                },
+                success: function (response) {
+                    $('#duration').append(response);
+                    console.log("Durations loaded:", response);
+                },
+                error: function (xhr, status, error) {
+                    console.log("Error loading durations:", error);
+                }
+            });
+        }
+    });
+
+    // When duration is selected
+    $('#duration').on('change', function () {
+        let courseId = $('#course').val();
+        let duration = $(this).val();
+
+        if (courseId && duration) {
+            $.ajax({
+                url: 'trainee.php',
+                type: 'POST',
+                data: {
+                    type: 'get_fee_discount',
+                    course_id: courseId,
+                    duration: duration
+                },
+                dataType: 'json',
+                success: function (response) {
+                    $('#fee').val(response.fee);
+                    $('#discount').val(response.discount);
+                },
+                error: function (xhr, status, error) {
+                    console.log("Error loading fee/discount:", error);
+                }
+            });
+        }
+    });
+});
+
+
+    // On Form Submit
+    $("#addTrainee").submit(function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        $.ajax({
+            url: 'action/trainee.php',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response == 'success') {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Trainee added successfully.',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    }).then(function () {
+                        $('#addTraineeModal').modal('hide');
+                        $('#addTrainee')[0].reset();
+                        location.reload();
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Error!',
+                        text: response,
+                        icon: 'error',
+                        confirmButtonText: 'Try Again'
+                    });
+                }
+            },
+            error: function (xhr, status, error) {
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'There was an issue with the request.',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                });
+            }
+        });
+    });
+
+
+
+
+
+// console.log("Trainee.js script is loaded");
+// $(document).ready(function() {
+//     $('#course').on('change', function () {
+//         let courseId = $(this).val();
+//         $('#duration').html('<option value="">-- Select Duration --</option>');
+//         $('#fee').val('');
+//         if (courseId) {
+//             $.ajax({
+//                 url: 'trainee.php',
+//                 type: 'POST',
+//                 data: { course_id: courseId },
+//                 success: function (response) {
+//                     $('#duration').append(response);
+//                 }
+//             });
+//         }
+//     });
+    
+//     $('#duration').on('change', function () {
+//         let courseId = $('#course').val();
+//         let duration = $(this).val();
+    
+//         if (courseId && duration) {
+//             $.ajax({
+//                 url: 'trainee.php',
+//                 type: 'POST',
+//                 data: {
+//                     course_id: courseId,
+//                     duration: duration
+//                 },
+//                 success: function (fee) {
+//                     $('#fee').val(fee);
+//                 }
+//             });
+//         }
+//     });
+    
+//     $("#addTrainee").submit(function(e) {
+//         e.preventDefault();
+//         var formData = new FormData(this);
+//         $.ajax({
+//             url: 'action/trainee.php', 
+//             type: 'POST',
+//             data: formData,
+//             contentType: false,  
+//             processData: false,  
+//             success: function(response) {
+//                 if (response == 'success') {
+//                     Swal.fire({
+//                         title: 'Success!',
+//                         text: 'Trainee added successfully.',
+//                         icon: 'success',
+//                         confirmButtonText: 'OK'
+//                     }).then(function() {
+//                         $('#addTraineeModal').modal('hide');
+//                         $('#addTrainee')[0].reset();
+//                         location.reload();
+//                     });
+//                 } else {
+//                     Swal.fire({
+//                         title: 'Error!',
+//                         text: response,
+//                         icon: 'error',
+//                         confirmButtonText: 'Try Again'
+//                     });
+//                 }
+//             },
+//             error: function(xhr, status, error) {
+//                 Swal.fire({
+//                     title: 'Error!',
+//                     text: 'There was an issue with the request.',
+//                     icon: 'error',
+//                     confirmButtonText: 'OK'
+//                 });
+//             }
+//         });
+//     });
+// });
+</script>
